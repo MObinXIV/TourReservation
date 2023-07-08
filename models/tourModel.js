1960e1e2cae5bcaf77dc656470ec5817f1a12708
MObinXIV <mobinxiv@gmail.com>
2023-07-08 10:50:10 +0300

    add first form of aggregation by using common grouping

--- models/tourModel.js
+++ models/tourModel.js
@@ -26,7 +26,7 @@ const tourSchema = new mongoose.Schema(
             type:Number,
             default:4.5
         },
-        ratingQuantity: {
+        ratingsQuantity: {
             type: Number,
             default: 0
         },
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -3,7 +3,9 @@ const tourController = require('../controllers/tourController');
 
 const router= express.Router();
 
-router.route('/top-5-cheap').get(tourController.aliasTopTours,tourController.getAllTours)
+router.route('/top-5-cheap').get(tourController.aliasTopTours,tourController.getAllTours);
+
+router.route('/tour-stats').get(tourController.getTourStats)
 
 router.route('/').post(tourController.createTour).get(tourController.getAllTours);
 router.route('/:id').get(tourController.getTour).patch(tourController.updateTour).delete(tourController.deleteTour);
