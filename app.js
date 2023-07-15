93b86beabd917a7516041b5fdb40fe70f2ac6193
MObinXIV <mobinxiv@gmail.com>
2023-07-15 17:31:09 +0300

    basic signUp

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ const express = require('express');
 const AppError= require('./utils/appError');
 const globalErrHandler= require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+
 const app  = express();
 
 app.use(express.json());
 
 
 app.use('/api/v1/tours',tourRouter);
+app.use('/api/v1/users', userRouter);
 
 app.all('*',(req,res,next)=>{
     
